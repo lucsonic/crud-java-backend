46980098ab62b1713c1ed03370a0a1c548803ad5
lucsonic <lucsonic@gmail.com>
2023-01-31 12:54:18 -0300

    Cadastro de cliente

--- backend/src/main/java/com/lsinformatica/crudjava/services/ClientService.java
+++ backend/src/main/java/com/lsinformatica/crudjava/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.lsinformatica.crudjava.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lsinformatica.crudjava.dto.ClientDTO;
 import com.lsinformatica.crudjava.entities.Client;
 import com.lsinformatica.crudjava.repositories.ClientRepository;
+import com.lsinformatica.crudjava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +24,28 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest);
 
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
+		return new ClientDTO(entity);
+	}
+
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setIncome(dto.getIncome());
+		entity.setChildren(dto.getChildren());
 	}
 }
