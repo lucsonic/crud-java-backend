f88f2c4b373488acb4887b15153bf6fa5fa779ce
lucsonic <lucsonic@gmail.com>
2023-01-31 13:14:37 -0300

    Update e Delete Cliente

--- backend/src/main/java/com/lsinformatica/crudjava/services/ClientService.java
+++ backend/src/main/java/com/lsinformatica/crudjava/services/ClientService.java
@@ -3,6 +3,7 @@ package com.lsinformatica.crudjava.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ import com.lsinformatica.crudjava.dto.ClientDTO;
 import com.lsinformatica.crudjava.entities.Client;
 import com.lsinformatica.crudjava.repositories.ClientRepository;
 import com.lsinformatica.crudjava.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -39,8 +42,29 @@ public class ClientService {
 		entity = repository.save(entity);
 
 		return new ClientDTO(entity);
+	}	
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado");
+		}
 	}
 
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado");
+		}
+	}
+	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
